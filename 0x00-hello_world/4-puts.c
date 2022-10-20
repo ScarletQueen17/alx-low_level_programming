519ebd583d331e985f5d45cc53ee74eeafbfb6cb
ScarletQueen17 <queenscarlet97@gmail.com>
2022-10-19 18:48:02 -0700

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
-
 /**
+ * main - print a line of code using puts
  *
- *  * main - entry point
- *
- *   *
- *
- *    * Return: Always 0
- *
- *     */
-
+ * Return: 0
+*/
 int main(void)
-
 {
-
-
-	puts("\"Programming is like building a multilingual puzzle");
-
+	puts("\"Programming is like biilding a multilingual puzzle");
 	return (0);
-
 }
